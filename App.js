7d479e41967a428bd8babb6df351552c25c86f88
lsouza <lsouza@luxfacta.com>
2021-07-17 20:30:03 -0300

    configure reactotron

--- App.js
+++ App.js
@@ -3,8 +3,14 @@ import "intl";
 import "intl/locale-data/jsonp/pt-BR";
 import Rotas from "./src/Rotas";
 import TelaPadrao from "./src/componentes/TelaPadrao";
+import Reactotron from "reactotron-react-native";
+
+Reactotron.configure().useReactNative().connect();
+console.tron = Reactotron;
 
 export default function App() {
+  console.tron.log('teste reactotron');
+
   return (
     <TelaPadrao>
       <Rotas />
